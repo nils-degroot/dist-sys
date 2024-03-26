aa0a06cd734fd235349fdcc625fce974cd53068d
Nils de Groot <nils@peeko.nl>
2024-03-26 14:56:30 +0100

    3c

--- broadcast/src/main.rs
+++ broadcast/src/main.rs
@@ -1,13 +1,14 @@
 use std::{
-    collections::{HashMap, HashSet},
+    collections::{HashMap, HashSet, VecDeque},
     sync::mpsc,
+    time::Instant,
 };
 
 use anyhow::{bail, Result};
 use dist_sys::{Body, Message, Node};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "snake_case", tag = "type")]
 enum Payload {
     Broadcast {
@@ -31,7 +32,7 @@ struct BroadcastNode {
     node_id: String,
     msg_id: usize,
     values: HashSet<usize>,
-    topology: HashMap<String, HashSet<String>>,
+    neighbors: HashSet<String>,
 }
 
 impl Node<Payload> for BroadcastNode {
@@ -46,70 +47,117 @@ impl Node<Payload> for BroadcastNode {
             node_id,
             msg_id: 0,
             values: HashSet::with_capacity(512),
-            topology: HashMap::with_capacity(16),
+            neighbors: HashSet::new(),
         }
     }
 
     fn run(&mut self) -> Result<()> {
-        while let Ok(msg) = self.rx.recv() {
-            let mut out = Message {
-                src: self.node_id.clone(),
-                dest: msg.src.clone(),
-                body: Body {
-                    msg_id: {
-                        let old = self.msg_id;
-                        self.msg_id += 1;
-                        Some(old)
-                    },
-                    in_reply_to: msg.body.msg_id,
-                    payload: Payload::TopologyOk,
-                },
-            };
+        let mut queue = VecDeque::<Message<Payload>>::with_capacity(16);
+        let mut unfinished = Vec::<(Instant, Message<Payload>)>::with_capacity(16);
 
-            match msg.body.payload {
-                Payload::Broadcast { message } => {
-                    if !self.values.contains(&message) {
-                        for child in self.topology.get(&self.node_id).unwrap_or(&HashSet::new()) {
-                            self.tx.send(Message {
-                                src: self.node_id.clone(),
-                                dest: child.clone(),
-                                body: Body {
-                                    msg_id: {
-                                        let old = self.msg_id;
-                                        self.msg_id += 1;
-                                        Some(old)
-                                    },
-                                    in_reply_to: None,
-                                    payload: Payload::Broadcast { message },
-                                },
-                            })?;
+        loop {
+            for (time, msg) in unfinished.iter_mut() {
+                if time.elapsed().as_millis() < 300 {
+                    continue;
+                }
+
+                self.tx.send(msg.clone())?;
+                *time = Instant::now();
+            }
+
+            if let Some(msg) = queue.pop_front() {
+                match msg.body.payload {
+                    Payload::Broadcast { message } => {
+                        let conf_msg = self.generate_message(
+                            Payload::BroadcastOk,
+                            msg.src.clone(),
+                            msg.body.msg_id,
+                        );
+                        self.tx.send(conf_msg)?;
+
+                        if self.values.contains(&message) {
+                            continue;
+                        }
+
+                        self.values.insert(message);
+
+                        for neighbor in self.neighbors.clone() {
+                            if neighbor == msg.src {
+                                continue;
+                            }
+
+                            let msg = self.generate_message(
+                                Payload::Broadcast { message },
+                                neighbor.to_string(),
+                                None,
+                            );
+
+                            self.tx.send(msg.clone())?;
+                            unfinished.push((Instant::now(), msg))
                         }
                     }
+                    Payload::Read => {
+                        let msg = self.generate_message(
+                            Payload::ReadOk {
+                                messages: self.values.clone(),
+                            },
+                            msg.src,
+                            msg.body.msg_id,
+                        );
+                        self.tx.send(msg)?;
+                    }
+                    Payload::Topology { topology } => {
+                        self.neighbors = topology[&self.node_id].clone();
 
-                    self.values.insert(message);
-                    out.body.payload = Payload::BroadcastOk;
+                        let msg =
+                            self.generate_message(Payload::TopologyOk, msg.src, msg.body.msg_id);
+                        self.tx.send(msg)?;
+                    }
+                    Payload::BroadcastOk => {
+                        let msg_id = msg.body.in_reply_to;
+                        let index = unfinished
+                            .iter()
+                            .position(|(_, msg)| msg_id == msg.body.msg_id);
 
-                    self.tx.send(out)?;
+                        if let Some(index) = index {
+                            unfinished.swap_remove(index);
+                        }
+                    }
+                    m => bail!("Message invalid for node: {m:?}"),
                 }
-                Payload::Read => {
-                    out.body.payload = Payload::ReadOk {
-                        messages: self.values.clone(),
-                    };
-
-                    self.tx.send(out)?;
-                }
-                Payload::Topology { topology } => {
-                    self.topology = topology;
-                    out.body.payload = Payload::TopologyOk;
-
-                    self.tx.send(out)?;
-                }
-                Payload::BroadcastOk => {}
-                _ => bail!("Message invalid for node"),
+            } else if let Ok(msg) = self.rx.recv() {
+                queue.push_back(msg);
+            } else {
+                break;
             }
         }
 
         Ok(())
+    }
+}
+
+impl BroadcastNode {
+    fn get_and_increment_id(&mut self) -> usize {
+        let old = self.msg_id;
+        self.msg_id += 1;
+        old
+    }
+
+    fn generate_message(
+        &mut self,
+        payload: Payload,
+        dest: String,
+        in_reply_to: Option<usize>,
+    ) -> Message<Payload> {
+        Message {
+            src: self.node_id.clone(),
+            dest,
+            body: Body {
+                msg_id: Some(self.get_and_increment_id()),
+                in_reply_to,
+                payload,
+            },
+        }
     }
 }
 
